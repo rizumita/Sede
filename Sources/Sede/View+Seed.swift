627ebfbf14d1aa7d091b00fd08bb954eb297f3f9
Ryoichi Izumita <r.izumita@caph.jp>
2021-05-13 05:05:05 +0900

    Remove unused code

--- Sources/Sede/View+Seed.swift
+++ Sources/Sede/View+Seed.swift
@@ -8,21 +8,5 @@ import SwiftUI
 public extension View {
     func sede<S>(_ seeder: S) -> some View where S: Seeder {
         modifier(seeder)
-//        modifier(InjectModifier(seeder: seeder))
-    }
-}
-
-@available(OSX 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
-private struct InjectModifier<S>: ViewModifier, DynamicProperty where S: Seeder {
-    @ObservedObject var object: SeederWrapper<S.Model, S.Msg>
-    var                 seeder: S
-
-    init(seeder: S) {
-        self.seeder = seeder
-        object = SeederWrapper(seeder: seeder)
-    }
-
-    func body(content: Content) -> some View {
-        content.environmentObject(object).modifier(seeder)
     }
 }
